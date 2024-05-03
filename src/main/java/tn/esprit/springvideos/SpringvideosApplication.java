28eaf63059e83e2f357d6f185836babcb9a03ca1
farouk-root <farouk.chalghoumi031@gmail.com>
2024-05-03 16:34:47 +0200

    init-video management

---
+++ src/main/java/tn/esprit/springvideos/SpringvideosApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.springvideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringvideosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringvideosApplication.class, args);
+    }
+
+}
